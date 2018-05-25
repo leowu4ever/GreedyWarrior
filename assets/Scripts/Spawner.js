bb3285ef53643521e898c81f5ac30eb40491ad23
Leo Wu <leowu4ever@users.noreply.github.com>
2018-05-25 22:40:51 +0100

    Update assets

--- assets/Scripts/Spawner.js
+++ assets/Scripts/Spawner.js
@@ -1,7 +1,7 @@
 var Spawner = cc.Class({
     name: 'Spawner',
     properties: {
-        enemy: cc.Prefab,
+        chest: cc.Prefab,
         left: cc.Node,
         canvas: cc.Node,
     },
@@ -11,16 +11,20 @@ var Spawner = cc.Class({
     },
 
     createEnemy () {
-        var creation = cc.instantiate (this.enemy);
-        //var scene = cc.director.getScene ();
-        
-        //find the correct parent for position
-
+        // create a chest
+        var creation = cc.instantiate (this.chest);        
         creation.parent = this.canvas;
         creation.setPosition (this.left.x, this.left.y);
         cc.log("create");
         var moveAlongLeft = cc.moveTo (3, creation.x, 1100);
         creation.runAction (moveAlongLeft);
+
+        // create a bat
+        //
+
+
+
+
     },
 
     start () {
