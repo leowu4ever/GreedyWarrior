7bd032192fe0c31fb3726545342b34ff3e8970c7
Leo Wu <leowu4ever@users.noreply.github.com>
2018-05-26 19:23:33 +0100

    Bat move and fire action
    - use callback function to connect multiple actions
    - find node position based on parents

--- assets/Scripts/Control.js
+++ assets/Scripts/Control.js
@@ -20,6 +20,11 @@ cc.Class({
         var jumpToLeft = cc.moveTo (speed, -319, this.warriorHorizontalPosition);
         var rotateToLeft = cc.scaleTo (speed, 8, 8);
 
+        // try spawn 
+        // make the node zoom while it moves upwards
+        // var spawn = cc.spawn(cc.moveBy(0.5, 0, 50), cc.scaleTo(0.5, 0.8, 1.4));
+        // node.runAction(spawn);
+
         if (this._isOnLeft) {
             this.warrior.runAction (jumpToRight);
             this.warrior.runAction (rotateToRight);
--- assets/Scripts/Spawner.js
+++ assets/Scripts/Spawner.js
@@ -7,14 +7,19 @@ var Spawner = cc.Class({
 
         bat: cc.Prefab,
         warrior: cc.Node,
+        test1: cc.Node,
     },
 
     ctor () {
         cc.log ("this is ctor??");
+
     },
 
     createEnemy () {
         // create a chest
+        cc.log ("test1 node space position" + this.test1.getParent ().convertToWorldSpace (this.test1.getPosition()));
+        cc.log ("test1 position" + this.test1.getPosition ());
+
         var chest = cc.instantiate (this.chest);        
         chest.parent = this.canvas;
         chest.setPosition (this.left.x, this.left.y);
@@ -23,34 +28,55 @@ var Spawner = cc.Class({
 
         // create a bat, then fly upwards, 
         // stay in the mid and immediately create a warning
-        // create arrow depending on warrior position
+        // create arrow depending on warrior positions
         // bat then explore
         // do sequence
 
         var bat = cc.instantiate (this.bat);
         bat.parent = this.canvas;
         bat.setPosition  (0, this.left.y);
-        var moveAlongMid = cc.moveTo (4, 0, -300);
-        bat.runAction (moveAlongMid);
-        var warning = bat.getChildByName ("Bat_Spawn_Warning");
-        warning.active = true;
-        var arrow = bat.getChildByName ("Bat_Arrow");
-        arrow.active = true;
-        var fireArrow = cc.moveTo (1, this.warrior.x, this.warrior.y);
+
+        var moveAlongMid = cc.moveTo (1, 0, -300);
+
+        var warning = bat.getChildByName ("Bat_Arrow_Warning");
+        var showWarning = cc.fadeTo (0.1, 255);
+
+        
+       
         // sequence
         
+        var callbackTest = function () {
+            warning.runAction (showWarning);
+        }
 
+        var callbackTest2 = function () {
+            var arrow = bat.getChildByName ("Bat_Arrow");
+            var showArrow = cc.fadeTo(0.1, 255); 
+            // get warrior position
+            // convert warrior position based on bat (parent)
+            var warriorWorldPos = this.warrior.getParent ().convertToWorldSpaceAR (this.warrior.getPosition());
+            var warriorNodePos = bat.convertToNodeSpaceAR (warriorWorldPos);
+            cc.log (warriorNodePos);
 
+            
 
-
-
-
-
-
+            var fireArrow = cc.moveTo (2, warriorNodePos.x, warriorNodePos.y);
+            
+            var sequence = cc.sequence (showArrow, fireArrow);
+            arrow.runAction(sequence);
+        }
+        
+        bat.runAction (cc.sequence (moveAlongMid, cc.callFunc (callbackTest, this), cc.callFunc (callbackTest2, this)));
     },
+
+
+
+
 
     start () {
         cc.log("it starts to spawn an enenmy");
+
+
     },
 
     move () {
